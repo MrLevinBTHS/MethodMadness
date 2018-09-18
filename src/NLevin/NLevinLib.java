c1d88ea193b2089a0c5c60f700ef168fb4827709
MrLevinBTHS <nlevin@schools.nyc.gov>
2018-09-18 09:47:23 -0400

    E-mail

---
+++ src/NLevin/NLevinLib.java
@@ -0,0 +1,9 @@
+package NLevin;
+
+public class NLevinLib {
+
+    public static void println(String str)
+    {
+        System.out.println(str);
+    }
+}
---
+++ src/NLevin/TestFile.java
@@ -0,0 +1,11 @@
+package NLevin;
+
+import static NLevin.NLevinLib.println;
+
+public class TestFile {
+
+    public static void main(String[] args)
+    {
+        println("I love a cup of Java in the morning.");
+    }
+}
