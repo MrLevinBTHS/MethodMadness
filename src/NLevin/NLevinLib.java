36673d9dc3daa39113600c3412cf1a4e66dba699
MrLevinBTHS <nlevin@schools.nyc.gov>
2018-09-20 08:33:15 -0400

    Fixed Email.

--- src/NLevin/NLevinLib.java
+++ src/NLevin/NLevinLib.java
@@ -6,4 +6,19 @@ public class NLevinLib {
     {
         System.out.println(str);
     }
+
+    /**
+     * Adds together the tens digits of the integer inputs.
+     * If the number is less than 10, use 0.
+     *
+     * @param num1 the first number to extract the tens digit from
+     * @param num2 the second number to extract the tens digit from
+     * @return     the sum of the tens digits added together
+     */
+    public int sumTensDigit(int num1, int num2)
+    {
+        //to be implemented
+        return 0;
+    }
+
 }
